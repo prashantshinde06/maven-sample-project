ad808a0b5be980c8413841c61ce7d8e913c63bf8
Prashant Shinde <shindeprashant3472@gmail.com>
2025-10-31 23:46:28 +0530

    new

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello, Maven project is working fine!");
+        System.out.println("Hello, Maven project is working fineee!");
     }
 }
